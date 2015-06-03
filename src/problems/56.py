5ac0cf525e26089257bbad42521aae7133a0d73c
Tennyson Holloway <tennyson.holloway@gmail.com>
2015-06-03 11:40:49 -0400

    remove old src

--- src/problems/56.py
+++
@@ -1,8 +0,0 @@
-max = 0
-for a in range(1, 101):
-    for b in range(1, 101):
-        num = a ** b
-        digitsum = sum(map(int,str(num)))
-        if digitsum > max:
-            max = digitsum
-print(max)
--- src/problems/63.py
+++
@@ -1,8 +0,0 @@
-count = 0
-for a in range(1, 300):
-    for b in range(1, 300):
-        c = a ** b
-        if len(str(c)) == b:
-            print(str(c)+"="+str(a)+"^"+str(b))
-            count+=1
-print(count)
